0bfee7ded9a3abee96ddfd0d3ad221bc8c091e28
halon <halon.filip@gmail.com>
2020-02-29 16:13:23 +0100

    Book, Author and ISBN models created and migrated.

--- bookshelfwise/books/models.py
+++ bookshelfwise/books/models.py
@@ -1,3 +1,31 @@
+# from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# import uuid
+
+
+# class User(AbstractUser):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class ISBN(models.Model):
+    number = models.CharField(max_length=32, unique=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.ManyToManyField(Author)
+    publication_date = models.DateField()
+    isbn = models.ManyToManyField(ISBN)
+    num_of_pages = models.IntegerField()
+    link_to_cover = models.URLField(blank=True)
+    publication_lang = models.CharField(max_length=32)
+
+    # class Meta:
+    #     constraints = [
+    #         models.UniqueConstraint(fields=['title', 'author', 'isbn'], name="different_isbns")
+    #     ]
