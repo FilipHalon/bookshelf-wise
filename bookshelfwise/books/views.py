9d0b3e421b0fbf585101dcf230f0c5a53746a876
halon <halon.filip@gmail.com>
2020-02-29 17:44:25 +0100

    BookList View along with template created and linked.

--- bookshelfwise/books/views.py
+++ bookshelfwise/books/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from books.models import Book
+
+
+class BookList(ListView):
+    model = Book
+    template_name = "books.html"
+    context_object_name = "books"
+    # paginate_by = ""
+    # page_kwarg = ""
+
+    # def get_queryset(self):
