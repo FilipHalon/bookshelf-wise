38ff9268603fe6c7888f779a25442a7501159479
halon <halon.filip@gmail.com>
2020-03-02 15:00:40 +0100

    BookCreateUpdateForm revised.

--- bookshelfwise/books/forms.py
+++ bookshelfwise/books/forms.py
@@ -1,33 +1,19 @@
 from django import forms
 
-from books.models import Book, Author, ISBN
-
-
-# class AuthorForm(ModelForm):
-#     class Meta:
-#         model = Author
-#         fields = '__all__'
-#
-#
-# class ISBNForm(ModelForm):
-#     class Meta:
-#         model = ISBN
-#         fields = '__all__'
+from books.models import Book
 
 
 class DateTypeInput(forms.DateInput):
     input_type = "date"
 
 
-class BookAddEditForm(forms.ModelForm):
-    # author = AuthorForm
-    author = forms.CharField(max_length=128)
-    isbn = forms.CharField(max_length=128)
-    # publication_date = forms.DateField()
+class BookCreateUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Book
         fields = '__all__'
         widgets = {
-            'publication_date': DateTypeInput
+            'publication_date': DateTypeInput,
+            'author': forms.TextInput,
+            'isbn': forms.TextInput
         }
