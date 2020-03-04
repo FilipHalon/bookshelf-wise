be7fda6dad46bccff176eebae23065fe2e515a26
halon <halon.filip@gmail.com>
2020-03-04 13:58:25 +0100

    Some more adjustments in tests - for the request factory.

--- bookshelfwise/books/tests.py
+++ bookshelfwise/books/tests.py
@@ -1,26 +1,45 @@
 from django.db import DataError
-from django.db.transaction import TransactionManagementError
 from django.test import TestCase, Client, TransactionTestCase
 from rest_framework.test import APIClient
 
 from books.models import Book, Author, ISBN
 
 example_book_attrs = {
-            "title": "Book",
+            "title": "Unique_Book_Title",
             "publication_date": "2020-03-01",
             "num_of_pages": 123,
             "link_to_cover": "https://en.wikipedia.org/wiki/Book#/media/File:Liji2_no_bg.png",
             "publication_lang": "en"
         }
 
-example_author_attrs = {"name": "Author"}
-example_isbn_attrs = {"number": "1234567890"}
+example_author_attrs = {"name": "Unique_Book_Author"}
+example_isbn_attrs = {"number": "1A2S3D4F5G6H"}
+
+
+def create_model_instance_each(self):
+    self.book = Book.objects.create(**example_book_attrs)
+    self.isbn = ISBN.objects.create(**example_isbn_attrs)
+    self.author = Author.objects.create(**example_author_attrs)
+    self.book.author.add(self.author)
+    self.book.isbn.add(self.isbn)
+
+
+def delete_model_instances(self):
+    self.book.delete()
+    self.isbn.delete()
+    self.author.delete()
+
 
 class BookListRequestTypeTestCase(TestCase):
+    c = Client()
+    url = "/books/"
+    filter_params = ["title__icontains", "publication_lang", "author", "from_date", "to_date"]
+
     def setUp(self):
-        self.c = Client()
-        self.url = "/books/"
-        self.filter_params = ["title__icontains", "publication_lang", "author", "from_date", "to_date"]
+        create_model_instance_each(self)
+
+    def tearDown(self):
+        delete_model_instances(self)
 
     def test_get_request_no_params_code_200(self):
         r = self.c.get(self.url)
@@ -41,6 +60,10 @@ class BookListRequestTypeTestCase(TestCase):
     def test_put_request_status_405(self):
         r = self.c.put(self.url)
         self.assertEqual(r.status_code, 405)
+
+
+class BookListViewTestCase(TestCase):
+    pass
 
 
 class BookModelsTestCase(TransactionTestCase):
@@ -64,7 +87,7 @@ class BookModelsTestCase(TransactionTestCase):
         self.book.title = "a" * 256
         self.assertEqual(self.book.title, "a" * 256)
 
-    def test_book_title_int_instead_of_str_no_errors(self):
+    def test_book_title_not_str_no_errors(self):
         self.book.title = 1
         self.book.save()
         self.assertTrue(self.book.pk)
@@ -72,8 +95,20 @@ class BookModelsTestCase(TransactionTestCase):
 
 
 class BookListEndpointTestCase(BookListRequestTypeTestCase):
+    c = APIClient()
+    url = "/api/books"
+    filter_params = ['title', 'author', 'isbn']
+    search_param = "search"
+
     def setUp(self):
-        self.c = APIClient()
-        self.url = "/api/books"
+        super().setUp()
+        self.query_params = [
+            {"title": self.book.title},
+            {"author": self.author.name},
+            {"isbn": self.isbn.number}
+        ]
 
-
+    # def test_filter_params_full_phrase_find_object(self):
+    #     for param in self.query_params:
+    #         r = self.c.get(self.url, param)
+    #         print(r)
