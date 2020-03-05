29684d0afbec34f631f435c3d23bfb589a3b19c9
halon <halon.filip@gmail.com>
2020-03-05 12:41:06 +0100

    Added settings and struture for staticfiles.

--- bookshelfwise/bookshelfwise/settings.py
+++ bookshelfwise/bookshelfwise/settings.py
@@ -127,7 +127,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # django REST framework
+
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
